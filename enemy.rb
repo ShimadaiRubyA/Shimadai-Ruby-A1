1bf076159ace173ef9ce8c72cbfcd0bced73d7b9
Hayato11422 <hayato11422@icloud.com>
2021-05-28 16:31:40 +0900

    敵のランダム移動2

--- enemy.rb
+++ enemy.rb
@@ -6,19 +6,30 @@ class Enemy < Sprite
   def update
     
     direction = rand(4)
+    speed = rand(20) * 5
 
     if direction == 0
-      self.x += 5
+      self.x += speed
 
     elsif direction == 1
-      self.x -= 5
+      self.x -= speed
 
     elsif direction == 2
-      self.y += 5
+      self.y += speed
 
     elsif direction == 3
-      self.y -= 5
-
+      self.y -= speed
     end
+
+    if self.x <= 0
+      self.x += speed
+    elsif self.x >= 640 - 32
+      self.x -= speed
+    elsif self.y <= 0
+      self.y += speed
+    elsif self.y >= 480 - 32
+      self.y -= speed
+    end
+
   end
 end
--- player.rb
+++ player.rb
@@ -3,14 +3,14 @@ class Player < Sprite
     self.x += Input.x * 3
     self.y += Input.y * 3
 
-    if self.x <= 32
-      self.x += 1
+    if self.x <= 0
+      self.x += 3
+    elsif self.x >= 640 - 32
+      self.x -= 3
+    elsif self.y <= 0
+      self.y += 3
     elsif self.y >= 480 - 32
-      self.x -= 1
-    elsif self.y <= 32
-      self.y += 1
-    elsif self.y >= 480 - 32
-      self.y -= 1
+      self.y -= 3
     end
   end
 end
