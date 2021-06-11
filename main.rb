9203524cf6ac40b34c9ad4e2baa51c574ec871e0
Yuto Tomita <touyu1999@outlook.jp>
2021-06-11 16:33:18 +0900

    スタート画面

--- main.rb
+++ main.rb
@@ -3,10 +3,13 @@ require 'dxruby'
 require_relative 'player'
 require_relative 'enemy'
 
+font = Font.new(32)
+
+
 player_img = Image.load("image/player.png")
 enemy_img = Image.load("image/enemy.png")
 
-player = Player.new(304, 380, player_img)
+player = Player.new()
 enemies = []
 10.times do
   enemies << Enemy.new(rand(0..(640 - 32 - 1)), rand((480 - 32 - 1)), enemy_img)
@@ -28,4 +31,22 @@ Window.loop do
 
     $i += 1
 
+    Window.draw_font(10, 10, "落ち物ゲーム　スコア：#{player.score}", font, {color: C_BLUE})
+
+    if !player.active # 以下追加
+      if player.game_end
+        Window.draw_font(210, 190, "ゲームオーバー", font, {color: C_BLUE})
+      end
+      Window.draw_font(120, 282, "スペースキー：ゲームスタート", font, {color: C_BLUE})
+      Window.draw_font(181, 314, "ESCキー：ゲーム終了", font, {color: C_BLUE})
+      if Input.key_push?(K_SPACE)
+        if player.game_end
+          enemies.map {|enemy| enemy.vanish}
+          Sprite.clean(enemies)
+        end
+        player.restart
+      elsif Input.key_push?(K_ESCAPE)
+        break
+      end
+    end
 end
--- player.rb
+++ player.rb
@@ -13,4 +13,39 @@ class Player < Sprite
       self.y -= 3
     end
   end
+ 
+  attr_accessor :score, :active, :game_end
+
+  def initialize()
+    image = Image.load("image/player.png")
+    x = (640 - image.width) / 2
+    y = 400 - image.height
+    @score = 0
+    @active = false # 変更
+    @game_end = false # 追加
+    super(x, y, image)
+  end
+
+  
+  def shot
+    @score += 1
+    if @score >= 7
+      @active = false
+      @game_end = true
+    end
+  end
+
+  def hit
+    @active = false
+    @game_end = true # 追加
+  end
+
+  def restart # 追加
+    self.x = (640 - image.width) / 2
+    self.y = 400 - image.height
+    @score = 0
+    @active = true
+    @game_end = false
+  end
+
 end
